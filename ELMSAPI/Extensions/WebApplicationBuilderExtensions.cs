0da3dd632f806b5dfc184da5bc55a3881c227ad5
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-24 14:14:48 +0400

    Configured application and Builder Extensions

---
+++ ELMSAPI/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ELMSAPI.Infrastructure.Extensions;
+
+namespace ELMSAPI.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder ConfigureApplicationBuilder(this WebApplicationBuilder builder)
+    {
+        #region Swagger
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        #endregion
+
+        #region Project Dependencies
+
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        #endregion
+
+        return builder;
+    }
+}
