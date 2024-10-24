181047b20ef90290fe75dac8a4e37fd42898985d
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-24 10:50:48 +0400

    Added LeaveStatus and LeaveType Models

---
+++ ELMSAPI.Domain/Models/LeaveStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace ELMSAPI.Domain.Models;
+public class LeaveStatus
+{
+    public required int Id { get; init; }
+    public required string Description { get; init; }
+    public DateTime DateCreated { get; init; }
+    public DateTime DateModified { get; set; }
+    public required string Status { get; set; }
+}
---
+++ ELMSAPI.Domain/Models/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace ELMSAPI.Domain.Models;
+public class LeaveType
+{
+    public required int Id { get; init; }
+    public required string Type { get; init; }
+    public DateTime DateCreated { get; init; }
+    public DateTime DateModified { get; set; }
+    public required string Status { get; set; }
+}
