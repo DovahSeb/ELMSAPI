efd715af5bc3e021682931a1b5343093d7b3ae9c
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-28 09:54:11 +0400

    Added Leave Statuses Repository

---
+++ ELMSAPI.Infrastructure/Repository/LeaveStatusesRepository.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using ELMSAPI.Application.LeaveStatuses;
+using ELMSAPI.Application.LeaveStatuses.DTOs;
+using ELMSAPI.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELMSAPI.Infrastructure.Repository;
+public sealed class LeaveStatusesRepository : ILeaveStatusesRepository
+{
+    private readonly DatabaseContext _context;
+    private readonly IMapper _mapper;
+
+    public LeaveStatusesRepository(DatabaseContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<LeaveStatusResponseDto>> GetLeaveStatuses(CancellationToken cancellationToken)
+    {
+        var leaveStatuses = await _context.LeaveStatuses
+            .Where(x => !x.Status.Equals("D"))
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        return _mapper.Map<List<LeaveStatusResponseDto>>(leaveStatuses);
+    }
+}
