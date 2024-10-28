4bc20275eef95277886946aa0134a0427b97856f
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-28 15:27:31 +0400

    Added LeaveTypes Repository Tests

---
+++ Tests/ELMSAPI.Infrastructure.Tests/Intergration/LeaveTypes/LeaveTypesRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using ELMSAPI.Application.LeaveTypes;
+using ELMSAPI.Application.LeaveTypes.DTOs;
+using ELMSAPI.Application.LeaveTypes.Queries.GetLeaveTypes;
+
+namespace ELMSAPI.Infrastructure.Tests.Intergration.LeaveTypes;
+
+public class LeaveTypesRepositoryTests
+{
+    private readonly ILeaveTypesRepository _leaveTypesRepository;
+    private GetLeaveTypesQuery _handler;
+
+    public LeaveTypesRepositoryTests()
+    {
+        _leaveTypesRepository = LeaveTypesMoq.GetLeaveTypesRepository();
+        _handler = new GetLeaveTypesQuery();
+    }
+
+    [Fact]
+    public async Task GetLeaveStatuses_ReturnsListOfLeaveStatuses()
+    {
+        var cancellationToken = new CancellationToken();
+
+        var result = await _leaveTypesRepository.GetLeaveTypes(cancellationToken);
+
+        Assert.NotNull(result);
+        Assert.IsType<List<LeaveTypeResponseDto>>(result);
+        Assert.Equal(5, result.Count);
+    }
+}
