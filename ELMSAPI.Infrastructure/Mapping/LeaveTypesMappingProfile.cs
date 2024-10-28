6c27bb933f8a1452076ee91a0356003a6ce53a68
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-28 15:40:22 +0400

    Added LeaveTypes Repository

---
+++ ELMSAPI.Infrastructure/Mapping/LeaveTypesMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ELMSAPI.Application.LeaveTypes.DTOs;
+using ELMSAPI.Domain.Models;
+
+namespace ELMSAPI.Infrastructure.Mapping;
+
+internal class LeaveTypesMappingProfile : Profile
+{
+    public LeaveTypesMappingProfile()
+    {
+        CreateMap<LeaveType, LeaveTypeResponseDto>();
+    }
+}
