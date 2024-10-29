c2e04d3d974138c51aeadff797788dfbd6103fcc
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-29 09:31:15 +0400

    Added GetLeaveTypes Queries

---
+++ ELMSAPI.Application/LeaveTypes/ILeaveTypesRepository.cs
@@ -0,0 +1,8 @@
+﻿using ELMSAPI.Application.LeaveTypes.DTOs;
+
+namespace ELMSAPI.Application.LeaveTypes;
+
+public interface ILeaveTypesRepository
+{
+    Task<List<LeaveTypeResponseDto>> GetLeaveTypes(CancellationToken cancellationToken);
+}
---
+++ ELMSAPI.Application/LeaveTypes/Queries/GetLeaveTypes/GetLeaveTypesHandler.cs
@@ -0,0 +1,11 @@
+﻿using ELMSAPI.Application.LeaveTypes.DTOs;
+using MediatR;
+
+namespace ELMSAPI.Application.LeaveTypes.Queries.GetLeaveTypes;
+public class GetLeaveTypesHandler(ILeaveTypesRepository repository) : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeResponseDto>>
+{
+    public async Task<List<LeaveTypeResponseDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        return await repository.GetLeaveTypes(cancellationToken);
+    }
+}
