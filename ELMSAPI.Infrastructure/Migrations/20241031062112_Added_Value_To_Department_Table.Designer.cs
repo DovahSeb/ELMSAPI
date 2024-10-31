d67ce9f4975cadbeb8a790690d3c1d285fe69d12
Sebastien Larue <sebastienlar619@gmail.com>
2024-10-31 10:57:35 +0400

    Seeded Reference Value in Department Table

--- ELMSAPI.Infrastructure/Migrations/20241031062112_Added_Value_To_Department_Table.Designer.cs
+++ ELMSAPI.Infrastructure/Migrations/20241031062112_Added_Value_To_Department_Table.Designer.cs
@@ -4,6 +4,7 @@ using ELMSAPI.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ELMSAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241031062112_Added_Value_To_Department_Table")]
+    partial class Added_Value_To_Department_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
