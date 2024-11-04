25d67b3e7d19e33a9e7b00e6665cd2f1ff825cb2
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-04 11:46:32 +0400

    Added GetEmployees Query With Tests

---
+++ ELMSAPI.Application/Employees/Queries/GetEmployees/GetEmployeesHandler.cs
@@ -0,0 +1,12 @@
+﻿using ELMSAPI.Application.Employees.DTOs;
+using MediatR;
+
+namespace ELMSAPI.Application.Employees.Queries.GetEmployees;
+
+public class GetEmployeesHandler(IEmployeesRepository employeesRepository) : IRequestHandler<GetEmployeesQuery, List<EmployeeResponseDto>>
+{
+    public async Task<List<EmployeeResponseDto>> Handle(GetEmployeesQuery request, CancellationToken cancellationToken)
+    {
+        return await employeesRepository.GetEmployees(cancellationToken);
+    }
+}
---
+++ ELMSAPI.Application/Employees/Queries/GetEmployees/GetEmployeesQuery.cs
@@ -0,0 +1,6 @@
+﻿using ELMSAPI.Application.Employees.DTOs;
+using MediatR;
+
+namespace ELMSAPI.Application.Employees.Queries.GetEmployees;
+
+public record GetEmployeesQuery : IRequest<List<EmployeeResponseDto>>;
