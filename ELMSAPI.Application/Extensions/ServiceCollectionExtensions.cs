d3e93fd966c3fde625fadae8cb7fda2db37133cf
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-14 11:29:06 +0400

    Register Validation and Request Response Behaviors

--- ELMSAPI.Application/Extensions/ServiceCollectionExtensions.cs
+++ ELMSAPI.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ELMSAPI.Application.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace ELMSAPI.Application.Extensions;
@@ -6,7 +8,13 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            cfg.AddOpenBehavior(typeof(RequestResponseLoggingBehavior<,>));
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
