e55ac6e44c8427dab27009abb9d7db4f95633c69
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-18 13:52:26 +0400

    Added GetDepartment Query and Handler

---
+++ ELMSAPI.Application/Departments/Queries/GetDepartments/GetDepartmentsQuery.cs
@@ -0,0 +1,6 @@
+﻿using ELMSAPI.Application.Departments.DTOs;
+using MediatR;
+
+namespace ELMSAPI.Application.Departments.Queries.GetDepartments;
+
+public record GetDepartmentsQuery : IRequest<List<DepartmentResponseDto>>;
