82d17296c570cf04ea71ccdf4fba229bd9f6cd3e
Sebastien Larue <sebastienlar619@gmail.com>
2024-11-18 13:49:44 +0400

    Added Departments Repository

---
+++ ELMSAPI.Infrastructure/Repository/DepartmentsRepository.cs
@@ -0,0 +1,36 @@
+﻿using ELMSAPI.Application.Departments;
+using ELMSAPI.Application.Departments.DTOs;
+using ELMSAPI.Infrastructure.Database;
+using ELMSAPI.Infrastructure.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELMSAPI.Infrastructure.Repository;
+
+public sealed class DepartmentsRepository : IDepartmentsRepository
+{
+    private readonly DatabaseContext _context;
+
+    public DepartmentsRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<DepartmentResponseDto>> GetDepartments(CancellationToken cancellationToken)
+    {
+        var departments = await _context.Departments
+            .Where(x => !x.Status.Equals("D"))
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+        var departmentDto = departments
+            .Select(x => x.MapToDepartmentResponseDto())
+            .ToList();
+
+        if (departmentDto.Count ==  0)
+        {
+            return [];
+        }
+
+        return departmentDto;
+    }
+}
